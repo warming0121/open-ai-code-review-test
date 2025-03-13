9b8a2e7ac29e34f4c8e9a99f3d6bb11e18ac9209
min <warming0121@163.com>
2025-03-13 13:55:17 +0800

    添加一些无用的

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -16,6 +16,15 @@ public class Main {
     }
 
 
+
+
+    public static void aaabbb(){
+        System.out.printf("aa");
+        wuyyyyyyyyyyyyy
+    }
+
+
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
